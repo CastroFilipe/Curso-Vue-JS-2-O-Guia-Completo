f288f9123b784f6b566416d46a69b8d91a7055a1
Filipe Castro <filipe.couto.10@gmail.com>
2020-02-15 17:27:19 -0300

    propriedades computadas

--- secao 2 - dom/desafio-03/app.js
+++ secao 2 - dom/desafio-03/app.js
@@ -5,12 +5,15 @@ new Vue({
     },
     computed:{
         resultado(){
+            console.log('computed...')
             return this.valor != 37 ? 'valor diferente de 37' : 'valor igual a 37'
         }
     },
     watch: {
         resultado(){
+            console.log('resultado...')
             setTimeout(()=> {
+                console.log('valor reiniciado')
                 this.valor = 0
             }, 5000)
         }
