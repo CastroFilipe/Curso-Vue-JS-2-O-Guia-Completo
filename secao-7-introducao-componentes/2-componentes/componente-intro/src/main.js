090739e7aaba25a431219598d96f406a02619bf5
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-03-09 16:33:16 -0300

    utilizando um contador simples como componente

--- secao-7-introducao-componentes/2-componentes/componente-intro/src/main.js
+++ secao-7-introducao-componentes/2-componentes/componente-intro/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Contador from './Contador.vue'//importando o componente criado
 
 Vue.config.productionTip = false
+Vue.component('app-contador', Contador)//referenciando o componente através da variável 'app-contador' de forma global
 
 new Vue({
   render: h => h(App),
