6da5714040ea8ae087fda849ffc6c0dd3294d3f6
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-03-12 17:47:37 -0300

    props no componente filho

--- secao-8-comunicacao-componentes/comunicacao/src/components/UsuarioInfo.vue
+++ secao-8-comunicacao-componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -2,13 +2,19 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do usuário: <strong>{{nome}}</strong></p>
+        <button @click="inverterNome">Inverter Nome</button>
+        <p>Nome do usuário: <strong>{{ nome }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['nome'] //define a propriedade que será aceita na tag referente a esse componente.
+    props: ['nome'], //define a propriedade que será aceita na tag referente a esse componente.
+    methods: {
+        inverterNome(){
+            this.nome = this.nome.split('').reverse().join('')
+        }
+    },
 }
 </script>
 
