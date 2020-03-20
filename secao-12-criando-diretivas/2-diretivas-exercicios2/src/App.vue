16ab03862059a5df5016f1fccabb3ffb292d07ed
Filipe Castro <filipe.couto.10@gmail.com>
2020-03-19 23:54:32 -0300

    usando múltiplos modificadores

--- secao-12-criando-diretivas/2-diretivas-exercicios2/src/App.vue
+++ secao-12-criando-diretivas/2-diretivas-exercicios2/src/App.vue
@@ -2,8 +2,11 @@
   <div id="app">
     <h1>DIRETIVAS</h1>
     <hr>
+	<p v-dir1="'yellow'">aplicação dir1</p>
 
-
+	<p v-dir2:fundo.atrasar.alternar="{cor1: 'green', cor2: 'blue', cor3: 'pink', atraso: 3000, intervalo: 500}">
+		aplicação de múltiplos modificadores
+	</p>
   </div>
 </template>
 
@@ -11,7 +14,42 @@
 
 
 export default {
+	//diretivas no componente local.
+	directives: {
+		dir1: {
+			bind(el, binding){
+				el.style.backgroundColor = binding.value
+			}
+		},
 
+		dir2: {
+			bind(el, binding){
+				const propriedades = binding.value
+
+				const aplicarCor = cor => {
+					if(binding.arg === 'fundo') {
+						el.style.backgroundColor = cor
+					} else {
+						el.style.color = cor
+					}
+				}
+
+				let corAtual = propriedades.cor2
+
+				setTimeout(()=>{	
+					if(binding.modifiers['alternar']){
+						setInterval(() => {
+							corAtual = corAtual === propriedades.cor2 ? propriedades.cor3 : propriedades.cor2
+							aplicarCor(corAtual)
+						}, propriedades.intervalo)
+					} else { //se não, aplicará a cor apenas uma vez
+						aplicarCor(corAtual)
+					}
+				}, propriedades.atraso)
+			
+			}
+		}
+	}
 }
 </script>
 
