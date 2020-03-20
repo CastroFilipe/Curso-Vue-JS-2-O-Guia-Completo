74a28958d81cb7779e65af6f22ce46e1907a1f7e
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-03-20 12:28:39 -0300

    criando filtro para formatar cpf

---
+++ secao-13-filtros-mixins/1-filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<!-- valor | filtro -->
+		<p>{{cpf | cpf}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	//criando filtro local para formatar cpf
+	filters: {
+		cpf(valor){
+			valor = valor.replace(/[^\d]/g, "")//remove qualquer caractere que não seja um dígito.
+
+			return valor.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+		}
+	},
+	data() {
+		return {
+			cpf: '10020030040'
+		}
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
