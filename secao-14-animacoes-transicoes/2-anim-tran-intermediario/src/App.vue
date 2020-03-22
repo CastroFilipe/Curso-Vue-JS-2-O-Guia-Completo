f5fa6c196f780d708faea5e4c5dfd4d3d849a938
Filipe Castro <filipe.couto.10@gmail.com>
2020-03-22 14:29:30 -0300

    segundo parte animações e transições

---
+++ secao-14-animacoes-transicoes/2-anim-tran-intermediario/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="exibirFrase = !exibirFrase">Mostrar frase</b-button>
+
+		<transition name="slide" appear><!--tag utilizada para transições-->
+			<b-alert variant="success" show class="mb-1" v-if="exibirFrase">Alerta 2 com transição: {{frase}}</b-alert>
+		</transition>
+
+		<transition name="slide" appear>
+			<b-alert variant="success" show v-if="exibirFrase">Alerta 3 com slide: {{frase}}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data(){
+		return {
+			frase: 'uma frase para o usuário',
+			exibirFrase: true
+		}
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+/*Os keyframes definem a animação. 
+Nesse caso, uma transformação de baixo para cima(slide-out) ou de cima para baixo(slide-in) */
+@keyframes slide-in {
+	from { transform: translateY(10px);}
+	to {transform: translateY(0);}
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0px);}
+	to {transform: translateY(5px);}
+}
+
+/*classes css que utilizam os frames criados*/ 
+.slide-enter-active {
+	animation: slide-in 0.5s ease;
+	transition: opacity 0.5s;
+}
+
+.slide-leave-active {
+	animation: slide-out 0.5s ease;
+	transition: opacity 0.5s;
+}
+
+.slide-enter, .slide-leave-to{
+	opacity: 0;
+}
+</style>
