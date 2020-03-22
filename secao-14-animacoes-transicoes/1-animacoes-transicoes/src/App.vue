b901976d0ced8397418bba6fa7e5da08fa670ac7
Filipe Castro <filipe.couto.10@gmail.com>
2020-03-22 12:32:08 -0300

    implementando fade como transição

--- secao-14-animacoes-transicoes/1-animacoes-transicoes/src/App.vue
+++ secao-14-animacoes-transicoes/1-animacoes-transicoes/src/App.vue
@@ -4,8 +4,10 @@
 		<hr>
 		<b-button variant="primary" class="mb-4" @click="exibirFrase = !exibirFrase">Mostrar frase</b-button>
 		
+		<b-alert variant="info" show v-if="exibirFrase">Alerta sem transição: {{frase}}</b-alert>
+
 		<transition name="meuEstilo"><!--tag utilizada para transições-->
-			<b-alert variant="info" show v-if="exibirFrase">{{frase}}</b-alert>
+			<b-alert variant="success" show v-if="exibirFrase">Alerta com transição: {{frase}}</b-alert>
 		</transition>
 	</div>
 </template>
@@ -34,26 +36,26 @@ export default {
 }
 /* padrão de nomeclatura para estilos aplicados em transições */
 .meuEstilo-enter {
-
+	opacity: 0;
 }
 
 .meuEstilo-enter-active {
-
+	transition: opacity 2s;
 }
 
 .meuEstilo-enter-to {
-
+	opacity: 1;
 }
 
 .meuEstilo-leave {
-
+	opacity: 1;
 }
 
 .meuEstilo-leave-active {
-
+	transition: opacity 2s;
 }
 
 .meuEstilo-leave-to {
-
+	opacity: 0;
 }
 </style>
