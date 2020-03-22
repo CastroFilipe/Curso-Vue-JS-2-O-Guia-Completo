4dba6da7aaabd551f315c4e49111cad96fbd46fa
Filipe Castro <filipe.couto.10@gmail.com>
2020-03-22 12:53:44 -0300

    animação com efeito 'slide'

--- secao-14-animacoes-transicoes/1-animacoes-transicoes/src/App.vue
+++ secao-14-animacoes-transicoes/1-animacoes-transicoes/src/App.vue
@@ -3,11 +3,13 @@
 		<h1>Animações</h1>
 		<hr>
 		<b-button variant="primary" class="mb-4" @click="exibirFrase = !exibirFrase">Mostrar frase</b-button>
-		
-		<b-alert variant="info" show v-if="exibirFrase">Alerta 1 sem transição: {{frase}}</b-alert>
 
 		<transition name="meuEstilo"><!--tag utilizada para transições-->
 			<b-alert variant="success" show v-if="exibirFrase">Alerta 2 com transição: {{frase}}</b-alert>
+		</transition>
+
+		<transition name="slide">
+			<b-alert variant="success" show v-if="exibirFrase">Alerta 3 com slide: {{frase}}</b-alert>
 		</transition>
 	</div>
 </template>
@@ -41,5 +43,28 @@ export default {
 
 .meuEstilo-enter-active, .meuEstilo-leave-active {
 	transition: opacity 2s;
+}
+
+/*TRECHO UTILIZADO PARA ANIMAÇÃO*/
+
+/*Os keyframes definem a animação. 
+Nesse caso, uma transformação de baixo para cima(slide-out) ou de cima para baixo(slide-in) */
+@keyframes slide-in {
+	from { transform: translateY(40px);}
+	to {transform: translateY(0);}
+}
+
+@keyframes slide-ou {
+	from { transform: translateY(0px);}
+	to {transform: translateY(40px);}
+}
+
+/*classes css que utilizam os frames criados*/ 
+.slide-enter-active {
+	animation: slide-in 1s ease;
+}
+
+.slide-leave-active {
+	animation: slide-ou 1s ease;
 }
 </style>
