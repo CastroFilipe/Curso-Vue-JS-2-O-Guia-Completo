5b5c59785899096036b453216e85eaebb6b96242
Filipe Castro <filipe.couto.10@gmail.com>
2020-03-22 12:03:14 -0300

    algumas classes e componentes do bootstrap

--- secao-14-animacoes-transicoes/1-animacoes-transicoes/src/App.vue
+++ secao-14-animacoes-transicoes/1-animacoes-transicoes/src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    
-  </div>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4" @click="exibirFrase = !exibirFrase">Mostrar frase</b-button>
+		<b-alert variant="info" show v-if="exibirFrase">{{frase}}</b-alert>
+
+	</div>
 </template>
 
 <script>
 
 export default {
-
+	data(){
+		return {
+			frase: 'uma frase para o usuário',
+			exibirFrase: false
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
 }
 </style>
