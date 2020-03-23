1e6ed578984117620f3cfd44158820cfab2ac203
Filipe Castro <filipe.couto.10@gmail.com>
2020-03-23 01:47:35 -0300

    criando formulário para inserir novos produtos

--- secao-15-servidor-via-http/1-http-exercicios/src/App.vue
+++ secao-15-servidor-via-http/1-http-exercicios/src/App.vue
@@ -1,6 +1,34 @@
 <template>
     <div id="app">
         <h1>HTTP com Axios</h1>
+        <b-card class="card-container">
+            <b-form-group label="Nome">
+                <b-form-input type="text" size="lg" v-model="produto.nome" 
+                placeholder="Informe o nome de um Hambúrguer ou refrigerante">
+                </b-form-input>
+            </b-form-group>
+            <!--  -->
+            <b-form-group label="Descrição">
+                <b-form-input type="text" size="lg" v-model="produto.descricao" 
+                placeholder="Informe a descrição">
+                </b-form-input>
+            </b-form-group>
+            <!--  -->
+            <b-form-group label="Preço">
+                <b-form-input type="number" size="lg" v-model="produto.preco" 
+                placeholder="Informe o valor">
+                </b-form-input>
+            </b-form-group>
+            <!--  -->
+            <b-form-group label="Categoria">
+                <div class="select-categorias" >
+                    <b-form-select v-model="selected" :options="categorias"></b-form-select>
+                </div>
+            </b-form-group>
+            <hr>
+            <b-button @click.prevent="salvar" 
+            size="lg" variant="success">Salvar</b-button>
+        </b-card>
     </div>
 </template>
 
@@ -10,7 +38,26 @@
 export default {
     data() {
         return {
-            
+            /*modelo de produto para refletir exatamente os modelos salvos 
+            no db.json. Com esse modelo poderão ser feitas chamadas POST para inserir novos 
+            produtos com os atributos setados e informados pelo usuário*/
+            produto : {
+                id: 0,
+                nome: '',
+                descricao:'',
+                preco: 0,
+                categoriaId: 1,
+            },
+            categorias: [
+                {value : 1, text: 'Hambúrgueres'},
+                {value : 2, text: 'Refrigerantes'}
+            ],
+            selected: ''
+        }
+    },
+    methods: {
+        salvar(){
+            console.log(this.produto)
         }
     },
 };
@@ -21,8 +68,21 @@ export default {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 20px;
+}
+
+.card-container {
+    width: 50%;
+    margin: 0px auto;
+}
+
+.select-categorias {
+    width: 30%;
 }
 </style>
