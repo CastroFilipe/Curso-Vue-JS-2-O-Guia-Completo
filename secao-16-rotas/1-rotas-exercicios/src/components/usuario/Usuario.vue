88ec875f9f92cf37f701f0bdfb0dbe20a5ecdbe3
Filipe Castro <filipe.couto.10@gmail.com>
2020-04-04 20:15:41 -0300

    Navegação via código com chamada de função

--- secao-16-rotas/1-rotas-exercicios/src/components/usuario/Usuario.vue
+++ secao-16-rotas/1-rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="usuario">
         <h2>Usuários</h2>
+        <hr>
+        <!-- o propriedade personalizada 'sucesso' foi definida apenas como uma forma alternativa 
+        de aplicar estilo. O estilo será definido no arquivo style.css presente na raiz do projeto-->
+        <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        /* O push fará a navegação para o path indicado*/
+        irParaInicio(){
+            // this.$router.push('/') //1° forma. Passando a string que representa o path
+            this.$router.push({path: '/'}) //forma alternativa. Passando um objeto com o atributo path
+        }  
+    },
 }
 </script>
 
